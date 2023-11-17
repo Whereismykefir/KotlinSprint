4705c0ce753627a9fa0899e531051f1fb5fdd39f
Whereismykefir <Whereismykefir@gmail.com>
2023-11-17 16:12:16 +0300

    KS-1-2 create and print stuff members

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+    var numberOfOrders: Int = 75
+    val thxMessage: String = "Спасибо за покупку"
+
+    println (numberOfOrders)
+    println(thxMessage)
+
+    var stuffMembers: Int = 2000
+
+//    println(stuffMembers)
+
+    stuffMembers = 1999
+    println(stuffMembers)
+}
