f1d7e07ce4121a55bb27cee32db9df501cc5cd12
Whereismykefir <Whereismykefir@gmail.com>
2023-11-21 14:42:32 +0300

    KS-2-2 create average wages stuff

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+fun main() {
+    var budgetStuffMember = 30000 * 50
+    var wageTotalBudget = 20000 * 30 + budgetStuffMember
+    var avgWage = wageTotalBudget / 80
+
+    println(budgetStuffMember)
+    println(wageTotalBudget)
+    println(avgWage)
+
+}
