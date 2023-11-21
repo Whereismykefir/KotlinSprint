a8620b14523923e04b18d84e3575c0fc16280467
Whereismykefir <Whereismykefir@gmail.com>
2023-11-21 13:04:08 +0300

    KS-2-1 average student score

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package lesson_2
+
+fun main() {
+    val student1 = 3.00
+    val student2 = 4.00
+    val student3 = 3.00
+    val student4 = 5.00
+    val avgScore = (student1 + student2 + student3 + student4) / 4
+
+    println(avgScore)
+}
