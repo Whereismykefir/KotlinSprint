4a1f882cb53a9b7100d1a1d53e07b2898fbfd3d1
Whereismykefir <Whereismykefir@gmail.com>
2023-11-22 09:01:24 +0300

    KS-1-5 refactor variables

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,8 +2,11 @@ package lesson_1
 
 fun main() {
     var seconds = 6480
-    var minutes = seconds / 60
-    var hours = minutes / 60
+    var minutes = ( seconds / 60 ) % 60
+    var hours = seconds / 3600
+    seconds = seconds % 60
 
-    println("0${hours}:${minutes % 60}:${seconds % 60}0 ")
+    val timeInSpace = "%02d:%02d:%02d".format(hours, minutes, seconds)
+
+    println(timeInSpace)
 }
