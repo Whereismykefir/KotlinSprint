efc0e6b86dfb7ab8b5bcc2a3c14baeed90a90ea3
Whereismykefir <Whereismykefir@gmail.com>
2023-11-22 13:26:55 +0300

    KS-2-3 create const one minute

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,12 +1,13 @@
 package lesson_2
 
 fun main() {
-    val startTime = 9 * 60 + 39
+    val startTime = 9 * oneMinute + 39
     val wayTime = 457
 
-    val finishHours = ( startTime + wayTime ) / 60
-    val finishMinutes = ( startTime + wayTime ) % 60
+    val finishHours = ( startTime + wayTime ) / oneMinute
+    val finishMinutes = ( startTime + wayTime ) % oneMinute
 
     println("$finishHours:$finishMinutes")
 
 }
+const val oneMinute = 60
