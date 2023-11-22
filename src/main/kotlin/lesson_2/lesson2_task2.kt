21d41cb52c5c419843e61cfbdd8be0a768a26c02
Whereismykefir <Whereismykefir@gmail.com>
2023-11-22 13:16:37 +0300

    KS-2-2 refactor variable type

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,9 +1,9 @@
 package lesson_2
 
 fun main() {
-    var budgetStuffMember = 30000 * 50
-    var wageTotalBudget = 20000 * 30 + budgetStuffMember
-    var avgWage = wageTotalBudget / 80
+    val budgetStuffMember = 30000 * 50
+    val wageTotalBudget = 20000 * 30 + budgetStuffMember
+    val avgWage = wageTotalBudget / 80
 
     println(budgetStuffMember)
     println(wageTotalBudget)
